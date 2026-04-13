ac8192a1bea27c0bd353a341fb77c2cfab693969
An Binh <lunarclientqt3333@gmail.com>
2026-04-13 20:29:39 +0700

    abcd

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
 // bình đẹp trai :)))
+
+const starsContainer = document.querySelector(".stars");
+
+for (let i = 0; i < 50; i++) {
+  const star = document.createElement("div");
+  star.classList.add("star");
+
+  star.style.left = Math.random() * 100 + "vw";
+  star.style.top = Math.random() * 100 + "vh";
+
+  star.style.animationDuration = (2 + Math.random() * 3) + "s";
+
+  starsContainer.appendChild(star);
+}
