9b2047d4bf0ebe674344d0a271efa99e1c0d1794
An Binh <lunarclientqt3333@gmail.com>
2026-04-15 19:13:00 +0700

    abcd

--- writing/script.js
+++ writing/script.js
@@ -143,12 +143,12 @@ function showPopup(link) {
 });
 
   popup.classList.remove("hidden");
-  document.body.style.overflow = "hidden";
+  document.body.style.overflow = "auto";
 }
 
 window.closePopup = function () {
   document.getElementById("popup").classList.add("hidden");
-  document.body.style.overflow = "auto";
+  document.body.style.overflow = "";
 };
 
 window.copyLink = function () {
