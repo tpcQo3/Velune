a9bc16f8910aa69c2aa5c8c6da2c1b5cdbd8b583
An Binh <lunarclientqt3333@gmail.com>
2026-04-18 18:53:38 +0700

    abcd

--- writing/modules/preview.js
+++ writing/modules/preview.js
@@ -6,12 +6,17 @@ const editor = document.getElementById("editor");
 const preview = document.getElementById("preview");
 
 export function updatePreview() {
+  let content = editor.innerHTML;
+
+  // fix xuống dòng chuẩn
+  content = content.replace(/<\/div>/g, "</div><br>");
+
   preview.innerHTML = `
     <div><b>Từ:</b> ${from.value || "..."}</div>
     <div><b>Đến:</b> ${to.value || "..."}</div>
     <hr>
     <div class="letter-content">
-      ${parseMarkdown(editor.innerText)}
+      ${content}
     </div>
   `;
 }
